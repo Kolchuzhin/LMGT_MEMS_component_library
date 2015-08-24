1dbf9ad7e5379177ffe4d44693ea6678357207ca
Vladimir <Kolchuzhin@users.noreply.github.com>
2015-08-24 20:50:12 +0200

    Update resistor.vhd

--- miscellaneous/resistor.vhd
+++ miscellaneous/resistor.vhd
@@ -1,1 +1,29 @@
+-------------------------------------------------------------------------------
+-- Model: resistor
+--
+-- Author: Vladimir Kolchuzhin, LMGT, TU Chemnitz
+-- <vladimir.kolchuzhin@ieee.org>
+--
+-- Date: 21.06.2011
 
+-- Library: kvl in hAMSter
+-------------------------------------------------------------------------------
+-- ID: resistor.vhd
+-- Rev. 1.0 
+-------------------------------------------------------------------------------
+use work.electromagnetic_system.all;
+use work.all;
+library ieee;
+
+
+entity resistor is
+  generic (resistance:real);      -- resistance value
+  port (terminal p,n:electrical); -- interface ports
+end entity resistor;
+
+
+architecture basic of resistor is
+  quantity v across i through p to n;
+begin
+	i == v/resistance;
+end architecture basic;
