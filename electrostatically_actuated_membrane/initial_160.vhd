edfe4d52ddf1d76f849f43a123f6d3a95253bef3
Vladimir <Kolchuzhin@users.noreply.github.com>
2015-10-23 13:07:08 +0200

    Update initial_160.vhd

--- electrostatically_actuated_membrane/initial_160.vhd
+++ electrostatically_actuated_membrane/initial_160.vhd
@@ -1,9 +1,11 @@
 package initial_160 is
 
 constant mm_1:real:=  0.306735124580E-07;
+constant km_1:real:=  23.67780511942;		-- ddE/dq1/dq1 at q1=0.0
 constant dm_1:real:=  2.840743666170e-05;	-- Q=30
 
 constant mm_2:real:=  0.201756803926E-07;
+constant km_2:real:=  212.6896178680;		-- ddE/dq2/dq2 at q2=0.0
 constant dm_2:real:=  6.905036631344e-05;	-- Q=30
 
 constant fi1_1:real:=   1.00000000000    ;
