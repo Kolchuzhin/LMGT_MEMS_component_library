90f32e18d9a8687a7517ff24105960a90e428278
Vladimir Kolchuzhin <Kolchuzhin@users.noreply.github.com>
2018-05-16 11:44:06 +0000

    Update capacitor.vhd

--- miscellaneous/capacitor.vhd
+++ miscellaneous/capacitor.vhd
@@ -1,1 +1,29 @@
+-------------------------------------------------------------------------------
+-- Model: capacitor
+--
+-- Author: Vladimir Kolchuzhin, LMGT, TU Chemnitz
+-- <vladimir.kolchuzhin@ieee.org>
+--
+-- Date: 21.06.2011
 
+-- Library: kvl in hAMSter
+-------------------------------------------------------------------------------
+-- ID: capacitor.vhd
+-- Rev. 1.0
+-------------------------------------------------------------------------------
+use work.electromagnetic_system.all;
+use work.all;
+library ieee;
+
+
+entity capacitor is
+  generic (capacitance:real);       -- capacitance value
+  port (terminal p,n:electrical); -- interface ports
+end entity capacitor;
+
+
+architecture basic of capacitor is
+  quantity v across i through p to n;
+begin
+        i == capacitance*v'dot;
+end architecture basic;
