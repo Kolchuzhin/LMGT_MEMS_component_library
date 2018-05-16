b1122f6e6ce838d3e0d7f8e692fc20e4fa993266
Vladimir Kolchuzhin <Kolchuzhin@users.noreply.github.com>
2018-05-16 11:34:24 +0000

    Update inductor.vhd

--- miscellaneous/inductor.vhd
+++ miscellaneous/inductor.vhd
@@ -1,1 +1,29 @@
+-------------------------------------------------------------------------------
+-- Model: inductor
+--
+-- Author: Vladimir Kolchuzhin, LMGT, TU Chemnitz 
+-- <vladimir.kolchuzhin@ieee.org> 
+-- 
+-- Date: 21.06.2011 
 
+-- Library: kvl in hAMSter
+-------------------------------------------------------------------------------
+-- ID: inductor.vhd
+-- Rev. 1.0  
+------------------------------------------------------------------------------- 
+use work.electromagnetic_system.all; 
+use work.all; 
+library ieee;
+
+
+entity inductor is
+  generic (inductance:real);      -- inductance value   
+  port (terminal p,n:electrical); -- interface ports 
+end entity inductor; 
+
+
+architecture basic of inductor is
+  quantity v across i through p to n;
+begin
+        v == inductance*i'dot;
+end architecture basic;
