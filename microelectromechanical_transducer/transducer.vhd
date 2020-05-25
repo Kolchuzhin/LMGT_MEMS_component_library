63d754fb5dbd3a644004f5036dd5797d102f675b
Vladimir Kolchuzhin <Kolchuzhin@users.noreply.github.com>
2020-05-25 19:20:40 +0200

    Update transducer.vhd

--- microelectromechanical_transducer/transducer.vhd
+++ microelectromechanical_transducer/transducer.vhd
@@ -24,12 +24,12 @@ use work.electromagnetic_system.all; -- hAMSter
 
 ENTITY transducer IS
 
--- abstract microelectomechanical tranducer parameters:
+-- abstract microelectomechanical transducer parameters:
 --  constant m:real:=4.658e-7;	-- mass
 --  constant d:real:=2.245e-3;	-- damping coefficient
---  constant c:real:=270.4;		-- spring constant
---  constant A:real:=1.0e-6;	-- plate area
---  constant h:real:=10.0e-6;	-- initial gap
+--  constant c:real:=270.4;		  -- spring constant
+--  constant A:real:=1.0e-6;	  -- plate area
+--  constant h:real:=10.0e-6;	  -- initial gap
 
   --generic (m,d,c,A,h:real);           -- generic tranducer parameters
 
@@ -55,25 +55,25 @@ ARCHITECTURE basic OF transducer IS
 -- set#1
 --  constant m:real:=4.658e-7;	-- mass
 --  constant d:real:=2.245e-3;	-- damping coefficient
---  constant c:real:=270.4;		-- spring constant
---  constant A:real:=1.0e-6;	-- plate area
---  constant h:real:=10.0e-6;	-- initial gap
+--  constant c:real:=270.4;		  -- spring constant
+--  constant A:real:=1.0e-6;	  -- plate area
+--  constant h:real:=10.0e-6;	  -- initial gap
 
 -- set#2
 -- [Release 11.0 Documentation for ANSYS: 7.4. Sample Electromechanical-Circuit Analysis, MKS unit]:
-  constant m:real:=1.0e-4;				-- mass, kg
-  constant d:real:=40.0e-3;				-- damping coefficient, uNs/um
-  constant c:real:=200.0;				-- spring constant, uN/um
+  constant m:real:=1.0e-4;				    -- mass, kg
+  constant d:real:=40.0e-3;				    -- damping coefficient, uNs/um
+  constant c:real:=200.0;				      -- spring constant, uN/um
   constant A:real:=(1.0e+8)*1.0e-12;	-- plate area, m^2
-  constant h:real:=150.0e-6;			-- initial gap, m
+  constant h:real:=150.0e-6;			    -- initial gap, m
 
 -- set#3
--- Übung: Microsystem design: Analysis of electromechanical system by M. Naumann
---  constant m:real:=1.0;	    		-- mass, kg
---  constant d:real:=0.0;				-- damping coefficient, Ns/m
---  constant c:real:=1.46;				-- spring constant, N/m
+-- Übung: Microsystem design: Analysis of electromechanical system by Prof. M. Naumann
+--  constant m:real:=1.0;	    		    -- mass, kg
+--  constant d:real:=0.0;				      -- damping coefficient, Ns/m
+--  constant c:real:=1.46;				    -- spring constant, N/m
 --  constant A:real:=50.0e-6*50.0e-6;	-- plate area, m*m
---  constant h:real:=3.0e-6;			-- initial gap, m
+--  constant h:real:=3.0e-6;			    -- initial gap, m
 
 BEGIN
 
