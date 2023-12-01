faa57112525ed4ad7611600ac7345123d445cb66
Vladimir Kolchuzhin <Kolchuzhin@users.noreply.github.com>
2023-12-01 16:19:08 +0100

    Update transducer.vhd

--- microelectromechanical_transducer/transducer.vhd
+++ microelectromechanical_transducer/transducer.vhd
@@ -25,16 +25,16 @@ use work.electromagnetic_system.all; -- hAMSter
 ENTITY transducer IS
 
 -- abstract microelectomechanical transducer parameters:
---  constant m:real:=4.658e-7;	-- mass
---  constant d:real:=2.245e-3;	-- damping coefficient
---  constant c:real:=270.4;		  -- spring constant
---  constant A:real:=1.0e-6;	  -- plate area
---  constant h:real:=10.0e-6;	  -- initial gap
+--  constant m:real:=4.658e-7;  -- mass
+--  constant d:real:=2.245e-3;  -- damping coefficient
+--  constant c:real:=270.4;     -- spring constant
+--  constant A:real:=1.0e-6;    -- plate area
+--  constant h:real:=10.0e-6;   -- initial gap
 
-  --generic (m,d,c,A,h:real);           -- generic tranducer parameters
+-- generic (m,d,c,A,h:real);    -- generic tranducer parameters
 
-  port (terminal n0,n1:electrical;		    -- electrical pins
-        terminal m0,m1:translational);		-- mechanical pins
+  port (terminal n0,n1:electrical;        -- electrical pins
+        terminal m0,m1:translational);    -- mechanical pins
 
 END ENTITY transducer;
 -------------------------------------------------------------------------------
@@ -47,33 +47,33 @@ ARCHITECTURE basic OF transducer IS
   quantity dcdu:real; -- Fel ~ dC/du
 
 -- MKS unit
-  constant eps:real:=8.85e-12;	-- permittivity F/m, MKS unit
+  constant eps:real:=8.85e-12;  -- permittivity F/m, MKS unit
 
 
 -- input parameters:
 
 -- set#1
---  constant m:real:=4.658e-7;	-- mass
---  constant d:real:=2.245e-3;	-- damping coefficient
---  constant c:real:=270.4;		  -- spring constant
---  constant A:real:=1.0e-6;	  -- plate area
---  constant h:real:=10.0e-6;	  -- initial gap
+--  constant m:real:=4.658e-7;  -- mass
+--  constant d:real:=2.245e-3;  -- damping coefficient
+--  constant c:real:=270.4;     -- spring constant
+--  constant A:real:=1.0e-6;    -- plate area
+--  constant h:real:=10.0e-6;   -- initial gap
 
 -- set#2
 -- [Release 11.0 Documentation for ANSYS: 7.4. Sample Electromechanical-Circuit Analysis, MKS unit]:
-  constant m:real:=1.0e-4;				    -- mass, kg
-  constant d:real:=40.0e-3;				    -- damping coefficient, uNs/um
-  constant c:real:=200.0;				      -- spring constant, uN/um
-  constant A:real:=(1.0e+8)*1.0e-12;	-- plate area, m^2
-  constant h:real:=150.0e-6;			    -- initial gap, m
+  constant m:real:=1.0e-4;            -- mass, kg
+  constant d:real:=40.0e-3;           -- damping coefficient, uNs/um
+  constant c:real:=200.0;             -- spring constant, uN/um
+  constant A:real:=(1.0e+8)*1.0e-12;  -- plate area, m^2
+  constant h:real:=150.0e-6;          -- initial gap, m
 
 -- set#3
 -- Übung: Microsystem design: Analysis of electromechanical system by Prof. M. Naumann
---  constant m:real:=1.0;	    		    -- mass, kg
---  constant d:real:=0.0;				      -- damping coefficient, Ns/m
---  constant c:real:=1.46;				    -- spring constant, N/m
---  constant A:real:=50.0e-6*50.0e-6;	-- plate area, m*m
---  constant h:real:=3.0e-6;			    -- initial gap, m
+--  constant m:real:=1.0;             -- mass, kg
+--  constant d:real:=0.0;             -- damping coefficient, Ns/m
+--  constant c:real:=1.46;            -- spring constant, N/m
+--  constant A:real:=50.0e-6*50.0e-6; -- plate area, m*m
+--  constant h:real:=3.0e-6;          -- initial gap, m
 
 BEGIN
 
